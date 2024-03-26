c7ce9b54a1de219c42859b5f95f0c01b71dd2d22
towelbro0812 <kevinjoy7271571@gmail.com>
2024-03-26 17:00:25 +0800

    add modal

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import streamlit as st
 import cv2
 import numpy as np
+from streamlit_modal import Modal
 
-img_file_buffer = st.camera_input("Take a picture")
+if "img_file_buffer" not in st.session_state:
+    st.session_state.img_file_buffer = None
 
-if img_file_buffer is not None:
+modal = Modal(
+    "Camera Modal",  
+    # Optional
+    padding=20,    # default value
+    max_width=744  # default value
+)
+if st.button("Open Camera"):
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.session_state.img_file_buffer = st.camera_input("Take a picture")
+    if st.session_state.img_file_buffer is not None:
+        modal.close()
+
+
+if st.session_state.img_file_buffer is not None:
     # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
+    bytes_data = st.session_state.img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
     # Check the type of cv2_img:
